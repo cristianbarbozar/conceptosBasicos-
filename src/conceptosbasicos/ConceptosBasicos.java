708a06f40a2e8600fcb67ccba661648cba09072a
cristianbarbozar <cristianbarboza398@aragon.unam.mx>
2021-03-21 21:33:28 -0600

    Tarea 22/03/2021

--- src/conceptosbasicos/ConceptosBasicos.java
+++ src/conceptosbasicos/ConceptosBasicos.java
@@ -18,14 +18,30 @@ public class ConceptosBasicos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Automovil miBocho= new Automovil();
-        miBocho.setMarca("VW");
-        miBocho.setSubMarca("Sedan");
-        miBocho.setModelo(1970);
-        miBocho.setColor(Color.RED);
-        System.out.println(miBocho);
+        Automovil miVocho= new Automovil();
+        miVocho.setMarca("VW");
+        miVocho.setSubMarca("Sedan");
+        miVocho.setModelo(1970);
+        miVocho.setColor(Color.RED);
+        System.out.println(miVocho);
         
        Automovil miAcura=new Automovil();
+       
+       miAcura.setMarca("Honda");
+       miAcura.setSubMarca("Acura");
+       miAcura.setModelo(1990);
+       miAcura.setColor(Color.BLUE);
+        System.out.println(miAcura);
+        
+       Automovil miMustang=new Automovil();
+       miMustang.setMarca("Ford");
+       miMustang.setSubMarca("Fastback ");
+               miMustang.setModelo(1964);
+               miMustang.setColor(Color.ORANGE);
+               System.out.println(miMustang);
+                       
+       
+       
     }
     
 }
